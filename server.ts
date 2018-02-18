fba3fd2e911e0bba17d6c4cfb1dfc9e6e04fb269
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2018-02-18 21:24:12 +0100

    feat(): support ssr

--- server.ts
+++ server.ts
@@ -1,14 +1,35 @@
-// These are important and needed before anything else
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2017-2018 Stefano Cappa (Ks89)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
-
 import { renderModuleFactory } from '@angular/platform-server';
 import { enableProdMode } from '@angular/core';
 
 import * as express from 'express';
 import { join } from 'path';
 import { readFileSync } from 'fs';
-// import * as socketIo from 'socket.io/lib/index.js';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -16,76 +37,116 @@ enableProdMode();
 // Express server
 const app = express();
 
-// const io = socketIo(app);
-
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // Our index.html we'll use as our template
 const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
 
+// ----------------------------------------------
+// ----------------------------------------------
+// workaround found here https://github.com/angular/universal/issues/830#issuecomment-345228799
+const domino = require('domino');
+const win = domino.createWindow(template);
+global['window'] = win;
+global['document'] = win.document;
+global['DOMTokenList'] = win.DOMTokenList;
+global['Node'] = win.Node;
+global['Text'] = win.Text;
+global['HTMLElement'] = win.HTMLElement;
+global['navigator'] = win.navigator;
+global['MutationObserver'] = getMockMutationObserver();
+
+function getMockMutationObserver() {
+  return class {
+    observe(node, options) {
+    }
+    disconnect() {
+    }
+    takeRecords() {
+      return [];
+    }
+  };
+}
+// ----------------------------------------------
+// ----------------------------------------------
+
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
 
-const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
+// Express Engine
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
+// Import module map for lazy loading
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-app.engine('html', (_, options, callback) => {
-  renderModuleFactory(AppServerModuleNgFactory, {
-    // Our index.html
-    document: template,
-    url: options.req.url,
-    // DI so that we can get lazy-loading to work differently (since we need it to just instantly render it)
-    extraProviders: [
-      provideModuleMap(LAZY_MODULE_MAP)
-    ]
-  }).then(html => {
-    callback(null, html);
-  });
-});
+// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+app.engine(
+  'html',
+  ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [provideModuleMap(LAZY_MODULE_MAP)]
+  })
+);
 
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
+/* - Example Express Rest API endpoints -
+  app.get('/api/**', (req, res) => { });
+*/
+
 // Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
+app.get(
+  '*.*',
+  express.static(join(DIST_FOLDER, 'browser'), {
+    maxAge: '1y'
+  })
+);
 
-// All regular routes use the Universal engine
+// ALl regular routes use the Universal engine
 app.get('*', (req, res) => {
-  res.render(join(DIST_FOLDER, 'browser', 'index.html'), { req });
-});
+  // ---------------------------------------------------
+  // part of the workaround found here https://github.com/angular/universal/issues/830#issuecomment-345228799
+  global['navigator'] = req['headers']['user-agent'];
+  // ---------------------------------------------------
 
-// io.on('connect', handleIO);
-//
-// io.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
-//
-// function handler(req, res) {
-//   res.writeHead(200);
-//   res.end();
-// }
-//
-// function handleIO(socket) {
-//   console.log('client connected');
-//   const intv = setInterval(() => {
-//     socket.emit('hello', Math.random());
-//   }, 1000);
-//
-//   socket.on('new-message', msg => {
-//     console.log('received a new message: ' + msg);
-//     socket.emit('message', msg);
-//   });
-//
-//   socket.on('disconnect', () => {
-//     console.log('client disconnected');
-//     clearInterval(intv);
-//   });
-// }
+  const http = req.headers['x-forwarded-proto'] === undefined ? 'http' : req.headers['x-forwarded-proto'];
+
+  // res.render('index', { req });
+
+  // tslint:disable-next-line:no-console
+  console.time(`GET: ${req.originalUrl}`);
+  res.render(
+    'index',
+    {
+      req: req,
+      res: res,
+      providers: [
+        {
+          provide: REQUEST, useValue: (req)
+        },
+        {
+          provide: RESPONSE, useValue: (res)
+        },
+        {
+          provide: 'ORIGIN_URL',
+          useValue: (`${http}://${req.headers.host}`)
+        }
+      ]
+    },
+    (err, html) => {
+      if (!!err) {
+        throw err;
+      }
+
+      // tslint:disable-next-line:no-console
+      console.timeEnd(`GET: ${req.originalUrl}`);
+      res.send(html);
+    });
+});
 
 // Start up the Node server
 app.listen(PORT, () => {
-  console.log(`Node server listening on http://localhost:${PORT}`);
+  console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
